7d8d3e177b2defb4ec7a8ad84788b6141129f66b
Clive B Harrison <clive_b_harrison@outlook.com>
2021-10-07 22:20:35 +0100

    Add project files.

---
+++ And Or Demo/Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class Form1
+
+    Private Sub btnOrCheck_Click(ByVal sender As System.Object, _
+        ByVal e As System.EventArgs) Handles btnOrCheck.Click
+
+        'Declare variables
+        Dim strName1 As String, strName2 As String
+
+        'Get the names
+        strName1 = txtName1.Text
+        strName2 = txtName2.Text
+
+        'Is one of the names Wendy?
+        If strName1 = "Wendy" Or strName2 = "Wendy" Then
+            MessageBox.Show("One of the names is Wendy.", _
+                "And Or Demo")
+        Else
+            MessageBox.Show("Neither of the names is Wendy.", _
+                "And Or Demo")
+        End If
+
+    End Sub
+
+    Private Sub btnAndCheck_Click(ByVal sender As System.Object, _
+        ByVal e As System.EventArgs) Handles btnAndCheck.Click
+
+        'Declare variables
+        Dim strName1 As String, strName2 As String
+
+        'Get the names
+        strName1 = txtName1.Text
+        strName2 = txtName2.Text
+
+        'Are both names Wendy?
+        If strName1 = "Wendy" And strName2 = "Wendy" Then
+            MessageBox.Show("Both names are Wendy.", _
+                "And Or Demo")
+        Else
+            MessageBox.Show("One of the names is not Wendy.", _
+                "And Or Demo")
+        End If
+
+    End Sub
+
+    Private Sub btnStringCompare_Click(ByVal sender As System.Object, _
+        ByVal e As System.EventArgs) Handles btnStringCompare.Click
+
+        'Declare variable
+        Dim strName As String
+
+        'Get the name
+        strName = txtName3.Text
+
+        'Compare the name
+        If String.Compare(strName, "BRYAN", True) = 0 Then
+            MessageBox.Show("Hello, Bryan!", "And Or Demo")
+        End If
+
+    End Sub
+End Class
---
+++ And Or Demo/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("And Or Demo")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("And Or Demo")> 
+<Assembly: AssemblyCopyright("Copyright ©  2009")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("43a7e7f0-4894-44d7-b2f5-d02c1b803445")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
